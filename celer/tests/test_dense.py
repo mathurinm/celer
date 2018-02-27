c5ee6e4150ab786e89da3df3efa96d1ef78f1e97
mathurinm <mathurin.massias@gmail.com>
2018-02-27 16:25:51 +0100

    test celer on sparse data

--- celer/tests/test_dense.py
+++ celer/tests/test_dense.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from scipy import sparse
+
 from celer import celer_path
 
 
@@ -22,8 +24,7 @@ def build_dataset(n_samples=50, n_features=200, n_informative_features=10,
     return X, y, X_test, y_test
 
 
-def test_enet_path_positive():
-    # Test positive parameter
+def test_celer_path_dense():
 
     X, y, _, _ = build_dataset(n_samples=50, n_features=50, n_targets=1)
 
@@ -33,5 +34,17 @@ def test_enet_path_positive():
 
     tol = 1e-6
     betas, thetas, gaps = celer_path(X, y, alphas=alphas, tol=tol, verbose=1)
-    assert True
-    # np.testing.assert_array_less(gaps, tol)
+    np.testing.assert_array_less(gaps, tol)
+
+
+def test_celer_path_sparse():
+
+    X, y, _, _ = build_dataset(n_samples=50, n_features=50, n_targets=1)
+    X = sparse.csc_matrix(X)
+    alpha_max = np.max(np.abs(X.T.dot(y)))
+    n_alphas = 10
+    alphas = alpha_max * np.logspace(0, -2, n_alphas)
+
+    tol = 1e-6
+    betas, thetas, gaps = celer_path(X, y, alphas=alphas, tol=tol, verbose=1)
+    np.testing.assert_array_less(gaps, tol)
