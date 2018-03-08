15f0a14c762b4329ca36600402a1a877624c9dd3
mathurinm <mathurin.massias@gmail.com>
2018-03-08 21:28:36 +0100

    Generate Pelican site

--- _downloads/plot_finance_path.py
+++ _downloads/plot_finance_path.py
@@ -1,37 +1,41 @@
 """
 =======================================================
-Lasso path computation on Leukemia dataset
+Lasso path computation on Finance/log1p dataset
 =======================================================
 
-The example runs the Celer algorithm on dense data.
+The example runs the Celer algorithm on sparse data.
 """
-
 import time
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import lasso_path
-from sklearn.datasets import fetch_mldata
+
+from scipy import sparse
 
 from celer import celer_path
+from celer.examples_utils import download_preprocess_finance
 from celer.plot_utils import plot_path_hist
 
 print(__doc__)
 
+print("Warning: this example may take more than 10 minutes to run")
 print("Loading data...")
-dataset = fetch_mldata("leukemia")
-X = np.asfortranarray(dataset.data.astype(float))
-y = dataset.target.astype(float)
-n_samples = len(y)
-
-y -= np.mean(y)
-y /= np.linalg.norm(y)
+try:
+    X = sparse.load_npz("./data/finance_data_preprocessed.npz")
+    y = np.load("./data/finance_target_preprocessed.npy")
+except FileNotFoundError:
+    print("Warning: downloading and preprocessing the Finance dataset.")
+    print("This may take several minutes")
+    download_preprocess_finance()
+    X = sparse.load_npz("./data/finance_data_preprocessed.npz")
+    y = np.load("./data/finance_target_preprocessed.npy")
 
 print("Starting path computation...")
 alpha_max = np.max(np.abs(X.T.dot(y)))
 
-fine = True  # fine or coarse grid
+# construct fine or coarse grid of regularization parameters
+fine = False
 n_alphas = 100 if fine else 10
 alphas = alpha_max * np.logspace(0, -2, n_alphas)
 
@@ -39,8 +43,9 @@ gap_freq = 10
 prune = 1
 verbose = 0
 verbose_inner = 0
-tols = [1e-2, 1e-4, 1e-6, 1e-8]
-results = np.zeros([2, len(tols)])
+
+tols = [1e-2, 1e-4, 1e-6]
+results = np.zeros([1, len(tols)])
 for tol_ix, tol in enumerate(tols):
     t0 = time.time()
     res = celer_path(X, y, alphas, max_iter=100, gap_freq=gap_freq,
@@ -48,15 +53,9 @@ for tol_ix, tol in enumerate(tols):
                      verbose_inner=verbose_inner,
                      tol=tol, prune=prune)
     results[0, tol_ix] = time.time() - t0
-    print('Celer time: %.2f s' % results[0, tol_ix])
     betas, thetas, gaps = res
 
-    t0 = time.time()
-    _, coefs, dual_gaps = lasso_path(X, y, tol=tol, alphas=alphas / n_samples)
-    results[1, tol_ix] = time.time() - t0
-    coefs = coefs.T
-
-labels = [r"\sc{CELER}", "scikit-learn"]
+labels = [r"\sc{CELER}"]
 figsize = (7, 4)
 fig = plot_path_hist(results, labels, tols, figsize, ylim=None)
 plt.show()
