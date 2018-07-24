670b5a1454b8dee47e77de7b69589319d8aa0886
mathurinm <mathurinm@users.noreply.github.com>
2018-07-24 11:38:29 +0200

    FIX: typo in docstring (#44)

--- celer/homotopy.py
+++ celer/homotopy.py
@@ -89,7 +89,7 @@ def celer_path(X, y, eps=1e-3, n_alphas=100, alphas=None,
 
     Returns
     -------
-    alphas : array, shape (n,_alphas,)
+    alphas : array, shape (n_alphas,)
         The alphas along the path where models are computed.
 
     coefs : array, shape (n_features, n_alphas)
