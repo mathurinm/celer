a241c6147f860b1c9a90882f1bb7fa274414c3b7
malcolmwolff <mlw32@uw.edu>
2018-09-17 12:15:50 -0700

    Modified warm_start slightly, added pytest warm start

--- celer/dropin_sklearn.py
+++ celer/dropin_sklearn.py
@@ -135,12 +135,15 @@ class Lasso(Lasso_sklearn):
 
     def path(self, X, y, alphas, **kwargs):
         """Compute Lasso path with Celer."""
+        if not self.warm_start:
+            self.coef_ = None
         alphas, coefs, dual_gaps = celer_path(
             X, y, alphas=alphas, max_iter=self.max_iter,
             gap_freq=self.gap_freq, max_epochs=self.max_epochs, p0=self.p0,
             verbose=self.verbose, tol=self.tol, prune=self.prune,
             X_scale=kwargs.get('X_scale', None),
-            X_offset=kwargs.get('X_offset', None))
+            X_offset=kwargs.get('X_offset', None),
+            coef_init=self.coef_)
         return (alphas, coefs, dual_gaps, [1])
 
 
@@ -249,7 +252,7 @@ class LassoCV(LassoCV_sklearn):
         super(LassoCV, self).__init__(
             eps=eps, n_alphas=n_alphas, alphas=alphas, max_iter=max_iter,
             tol=tol, cv=cv, fit_intercept=fit_intercept, normalize=normalize,
-             verbose=verbose)
+            verbose=verbose)
         self.gap_freq = gap_freq
         self.max_epochs = max_epochs
         self.p0 = p0
