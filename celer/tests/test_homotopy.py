38b6a56a415af3dc5b857235222bfb51029fe526
malcolmwolff <mlw32@uw.edu>
2018-09-17 12:20:38 -0700

    Added warm start test to test_homotopy.py

--- celer/tests/test_homotopy.py
+++ celer/tests/test_homotopy.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import pytest
+import time
 
 from itertools import product
 from scipy import sparse
@@ -142,3 +143,42 @@ def test_zero_column(sparse_X):
     np.testing.assert_array_less(gaps[-1], tol)
     np.testing.assert_equal(w.shape[0], X.shape[1])
     np.testing.assert_equal(theta.shape[0], X.shape[0])
+
+
+@pytest.mark.parametrize("sparse_X", [False, True])
+def test_warm_start(sparse_X):
+    """Test Lasso path convergence."""
+    X, y, _, _ = build_dataset(n_samples=30, n_features=50, sparse_X=sparse_X)
+    n_samples, n_features = X.shape
+    alpha_max = np.max(np.abs(X.T.dot(y))) / n_samples
+    n_alphas = 10
+    alphas = alpha_max * np.logspace(0, -2, n_alphas)
+    tol = 1e-6
+
+    reg1 = Lasso(alpha=alphas[0], tol=tol, warm_start=True)
+    reg2 = Lasso(alpha=alphas[0], tol=tol, warm_start=False)
+    reg1.coef_ = np.zeros(n_features)
+
+    reg1.fit(X, y)
+    reg2.fit(X, y)
+
+    ws_time_start = time.time()
+
+    for k in range(1, len(alphas)):
+        coef1 = reg1.coef_
+        reg1 = Lasso(alpha=alphas[k], tol=tol, warm_start=True)
+        reg1.coef_ = coef1
+
+        reg1.fit(X, y)
+
+    ws_time = time.time() - ws_time_start
+
+    nws_time_start = time.time()
+
+    for k in range(1, len(alphas)):
+        reg2 = Lasso(alpha=alphas[k], tol=tol, warm_start=False)
+        reg2.fit(X, y)
+
+    nws_time = time.time() - nws_time_start
+
+    assert ws_time < nws_time
