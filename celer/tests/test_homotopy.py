53abc4393f80ea338d0ec41fab52b09c94e502ad
Hicham Janati <hicham.janati@inria.fr>
2018-09-19 16:55:16 +0200

    test positive for Lasso and LassoCV

--- celer/tests/test_homotopy.py
+++ celer/tests/test_homotopy.py
@@ -69,9 +69,9 @@ def test_celer_path_vs_lasso_path(sparse_X, prune):
     np.testing.assert_allclose(coefs1, coefs2, rtol=1e-05, atol=1e-6)
 
 
-@pytest.mark.parametrize("sparse_X, fit_intercept", product([False, True],
-                                                            [False, True]))
-def test_dropin_LassoCV(sparse_X, fit_intercept):
+@pytest.mark.parametrize("sparse_X, fit_intercept", "positive",
+                         product([False, True], [False, True], [False, True]))
+def test_dropin_LassoCV(sparse_X, fit_intercept, positive):
     """Test that our LassoCV behaves like sklearn's LassoCV."""
     X, y, _, _ = build_dataset(n_samples=30, n_features=50, sparse_X=sparse_X)
     params = dict(eps=1e-1, n_alphas=100, tol=1e-10, cv=2,
@@ -93,13 +93,16 @@ def test_dropin_LassoCV(sparse_X, fit_intercept):
     check_estimator(LassoCV)
 
 
-@pytest.mark.parametrize("sparse_X, fit_intercept", product([False, True],
-                                                            [False, True]))
-def test_dropin_lasso(sparse_X, fit_intercept):
+@pytest.mark.parametrize("sparse_X, fit_intercept", "positive",
+                         product([False, True], [False, True], [False, True]))
+def test_dropin_lasso(sparse_X, fit_intercept, positive):
     """Test that our Lasso class behaves as sklearn's Lasso."""
     X, y, _, _ = build_dataset(n_samples=20, n_features=30, sparse_X=sparse_X)
+    if not positive:
+        alpha_max = np.linalg.norm(X.T.dot(y), ord=np.inf) / X.shape[0]
+    else:
+        alpha_max = X.T.dot(y).max() / X.shape[0]
 
-    alpha_max = np.linalg.norm(X.T.dot(y), ord=np.inf) / X.shape[0]
     alpha = alpha_max / 2.
     params = dict(alpha=alpha, fit_intercept=fit_intercept, tol=1e-10,
                   normalize=True)
@@ -142,28 +145,3 @@ def test_zero_column(sparse_X):
     np.testing.assert_array_less(gaps[-1], tol)
     np.testing.assert_equal(w.shape[0], X.shape[1])
     np.testing.assert_equal(theta.shape[0], X.shape[0])
-
-
-@pytest.mark.parametrize("sparse_X, fit_intercept", product([False, True],
-                                                            [False, True]))
-def test_positive_lasso(sparse_X, fit_intercept):
-    """Test that our Lasso class behaves as sklearn's Lasso."""
-    X, y, _, _ = build_dataset(n_samples=20, n_features=30, sparse_X=sparse_X)
-
-    alpha_max = (X.T.dot(y)).max() / X.shape[0]
-    alpha = alpha_max / 2.
-    params = dict(alpha=alpha, fit_intercept=fit_intercept, tol=1e-10,
-                  normalize=True, positive=True)
-    clf = Lasso(**params)
-    clf.fit(X, y)
-
-    clf2 = sklearn_Lasso(**params)
-    clf2.fit(X, y)
-
-    assert (clf.coef_ >= 0.).all()
-
-    np.testing.assert_allclose(clf.coef_, clf2.coef_, rtol=1e-5)
-    if fit_intercept:
-        np.testing.assert_allclose(clf.intercept_, clf2.intercept_)
-
-    check_estimator(Lasso)
