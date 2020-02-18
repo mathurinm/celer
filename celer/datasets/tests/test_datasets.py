39a0c509324a2803e887b3a6708ac11c3dd1d1d9
mathurinm <mathurin.massias@gmail.com>
2020-02-18 13:25:57 +0100

    comment out climate test

--- celer/datasets/tests/test_datasets.py
+++ celer/datasets/tests/test_datasets.py
@@ -1,22 +1,17 @@
 import numpy as np
 
-from celer.datasets import load_libsvm, load_climate
-# from celer.datasets.libsvm import NAMES
+from celer.datasets import load_libsvm
 
 
 def test_news20():
     # other datasets are super big
     # dataset = np.random.choice(list(NAMES.keys()))
     # print(dataset)
-    # dataset = 'news20'
-    # X, y = load_libsvm(dataset)
+    dataset = 'news20'
+    X, y = load_libsvm(dataset)
     np.testing.assert_equal(X.shape[0], y.shape[0])
 
 
-def test_climate():
+# def test_climate():
     # X, y = load_climate()
     # np.testing.assert_equal(X.shape[0], y.shape[0])
-    try:
-        1 / 0
-    except ZeroDivisionError:
-        pass
