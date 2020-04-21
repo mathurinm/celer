334152b1dbed355a03c011f94a04732690da0fd1
yjkimnada <30935933+yjkimnada@users.noreply.github.com>
2020-04-21 12:27:14 -0400

    Multi-CPU for LassoCV in dropin_sklearn.py

--- celer/dropin_sklearn.py
+++ celer/dropin_sklearn.py
@@ -269,11 +269,11 @@ class LassoCV(LassoCV_sklearn):
                  fit_intercept=True, normalize=False, max_iter=100,
                  tol=1e-4, cv=None, verbose=0, gap_freq=10,
                  max_epochs=50000, p0=10, prune=0, precompute='auto',
-                 positive=False):
+                 positive=False, n_jobs=1):
         super(LassoCV, self).__init__(
             eps=eps, n_alphas=n_alphas, alphas=alphas, max_iter=max_iter,
             tol=tol, cv=cv, fit_intercept=fit_intercept, normalize=normalize,
-            verbose=verbose)
+            verbose=verbose, n_jobs=n_jobs)
         self.gap_freq = gap_freq
         self.max_epochs = max_epochs
         self.p0 = p0
