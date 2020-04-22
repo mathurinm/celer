b81c6b5dee9d1bb0d140cb86f55c69c27c0cc24b
mathurinm <mathurin.massias@gmail.com>
2020-04-22 09:10:48 +0200

    use logscale plot for alpha in CV example

--- examples/plot_lasso_cv.py
+++ examples/plot_lasso_cv.py
@@ -20,7 +20,6 @@ print("Loading data...")
 dataset = fetch_openml("leukemia")
 X = np.asfortranarray(dataset.data.astype(float))
 y = 2 * ((dataset.target == "AML") - 0.5)
-n_samples = len(y)
 
 model = LassoCV(cv=3)
 model.fit(X, y)
@@ -28,18 +27,17 @@ model.fit(X, y)
 print("Estimated regularization parameter alpha: %s" % model.alpha_)
 
 # Display results
-m_log_alphas = -np.log10(model.alphas_)
 
 plt.figure()
-plt.plot(m_log_alphas, model.mse_path_, ':')
-plt.plot(m_log_alphas, model.mse_path_.mean(axis=-1), 'k',
-         label='Average across the folds', linewidth=2)
-plt.axvline(-np.log10(model.alpha_), linestyle='--', color='k',
+plt.semilogx(model.alphas_, model.mse_path_, ':')
+plt.semilogx(model.alphas_, model.mse_path_.mean(axis=-1), 'k',
+             label='Average across the folds', linewidth=2)
+plt.axvline(model.alpha_, linestyle='--', color='k',
             label='alpha: CV estimate')
 
 plt.legend()
 
-plt.xlabel('-log(alpha)')
+plt.xlabel(r'$\alpha$')
 plt.ylabel('Mean square error')
 plt.axis('tight')
 plt.show()
