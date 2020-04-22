575b14e75aaddaa653b3fb4b3a11aec54b5d6df7
yjkimnada <30935933+yjkimnada@users.noreply.github.com>
2020-04-22 04:47:03 -0400

    [MRG] Multi-CPU for LassoCV in dropin_sklearn.py (#117)
    * Multi-CPU for LassoCV in dropin_sklearn.py
    
    * Use None as default value as in sklearn and fix docstring test
    
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>

--- celer/dropin_sklearn.py
+++ celer/dropin_sklearn.py
@@ -234,6 +234,11 @@ class LassoCV(LassoCV_sklearn):
     positive : bool, optional (default=False)
         When set to True, forces the coefficients to be positive.
 
+    n_jobs : int or None, optional (default=None)
+        Number of CPUs to use during the cross validation.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors.
+
     Attributes
     ----------
     alpha_ : float
@@ -269,11 +274,11 @@ class LassoCV(LassoCV_sklearn):
                  fit_intercept=True, normalize=False, max_iter=100,
                  tol=1e-4, cv=None, verbose=0, gap_freq=10,
                  max_epochs=50000, p0=10, prune=0, precompute='auto',
-                 positive=False):
+                 positive=False, n_jobs=None):
         super(LassoCV, self).__init__(
             eps=eps, n_alphas=n_alphas, alphas=alphas, max_iter=max_iter,
             tol=tol, cv=cv, fit_intercept=fit_intercept, normalize=normalize,
-            verbose=verbose)
+            verbose=verbose, n_jobs=n_jobs)
         self.gap_freq = gap_freq
         self.max_epochs = max_epochs
         self.p0 = p0
