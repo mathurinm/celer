Fix scaling of dual point

celer/homotopy.py
@@ -397,7 +397,7 @@ def mtl_path(
 
     norms_X_col = np.linalg.norm(X, axis=0)
     Y = np.asfortranarray(Y)
-    # R = Y.copy(order='F')
+    R = Y.copy(order='F')
     theta = np.zeros_like(Y, order='F')
 
     # do not skip alphas[0], it is not always alpha_max
@@ -415,18 +415,7 @@ def mtl_path(
         else:
             W = coefs[:, :, t].copy()
             p_t = 10
-        print("norm W, ",  norm(W))
 
-        W = np.zeros([n_features, n_tasks], order='C')
-
-        R = np.asfortranarray(Y - X @ W)
-        print(Y[0, :])
-        print(Y[0, :].shape)
-        print(Y.shape)
-
-        alpha = alphas[t]
-
-        print("o ", norm(R) ** 2 / len(R) / 2 + alpha * norm(W, axis=1).sum())
         sol = celer_mtl(
             X, Y, alpha, W, R, theta, norms_X_col, p0=p_t, tol=tol,
             prune=prune, max_iter=max_iter, max_epochs=max_epochs,