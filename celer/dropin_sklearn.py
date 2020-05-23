c17670bc6ddee3cf1db8370a47634bfc53d26fdc
mathurinm <mathurin.massias@gmail.com>
2020-05-23 08:44:17 +0200

    handle verbose_inner as max(0,verbose-1)

--- celer/dropin_sklearn.py
+++ celer/dropin_sklearn.py
@@ -417,8 +417,8 @@ class MultiTaskLasso(MultiTaskLasso_sklearn):
             X, y, alphas=[self.alpha], coef_init=self.coef_,
             max_iter=self.max_iter, gap_freq=self.gap_freq,
             max_epochs=self.max_epochs, p0=self.p0, verbose=self.verbose,
-            verbose_inner=self.verbose-1,
-            tol=self.tol, prune=self.prune)
+            verbose_inner=max(0, self.verbose-1), tol=self.tol,
+            prune=self.prune)
 
         self.coef_, self.dual_gap_ = coefs[..., 0], dual_gaps[-1]
         self.n_iter_ = len(dual_gaps)
@@ -547,10 +547,9 @@ class MultiTaskLassoCV(RegressorMixin, sklearn_LinearModelCV):
         """Compute Lasso path with Celer."""
 
         alphas, coefs, dual_gaps = mtl_path(
-            X, y, alphas=alphas, coef_init=coef_init,
-            max_iter=self.max_iter, gap_freq=self.gap_freq,
-            max_epochs=self.max_epochs, p0=self.p0, verbose=self.verbose,
-            verbose_inner=self.verbose-1,
+            X, y, alphas=alphas, coef_init=coef_init, max_iter=self.max_iter,
+            gap_freq=self.gap_freq, max_epochs=self.max_epochs, p0=self.p0,
+            verbose=self.verbose, verbose_inner=max(0, self.verbose-1),
             tol=self.tol, prune=self.prune)
 
         return alphas, coefs, dual_gaps
