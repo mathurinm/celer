72ce0d69f50c89ad5b074447d3c59bf1488c3d16
mathurinm <mathurin.massias@gmail.com>
2020-08-26 13:13:32 +0200

    Use pandas instead of custom function to plot timings histograms

--- celer/plot_utils.py
+++ celer/plot_utils.py
@@ -29,27 +29,3 @@ def configure_plt():
     sns.set_palette('colorblind')
     sns.set_context("poster")
     sns.set_style("ticks")
-
-
-def plot_path_hist(results, labels, tols, figsize, ylim=None):
-    configure_plt()
-    sns.set_palette('colorblind')
-    n_competitors = len(results)
-    fig, ax = plt.subplots(figsize=figsize)
-    width = 1. / (n_competitors + 1)
-    ind = np.arange(len(tols))
-    b = (1 - n_competitors) / 2.
-    for i in range(n_competitors):
-        plt.bar(ind + (i + b) * width, results[i], width,
-                label=labels[i])
-    ax.set_ylabel('path computation time (s)')
-    ax.set_xticks(ind + width / 2)
-    plt.xticks(range(len(tols)), ["%.0e" % tol for tol in tols])
-    if ylim is not None:
-        plt.ylim(ylim)
-
-    ax.set_xlabel(r"$\epsilon$")
-    plt.legend(loc='upper left')
-    plt.tight_layout()
-    plt.show(block=False)
-    return fig
