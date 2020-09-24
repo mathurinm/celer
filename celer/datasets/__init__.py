37967e628c44ec9df34c0ac10647bbd9a9402935
mathurinm <mathurin.massias@gmail.com>
2020-09-24 13:08:24 +0200

    save autoformat induces error

--- celer/datasets/__init__.py
+++ celer/datasets/__init__.py
@@ -1,5 +1,7 @@
-from .ml_uci import fetch_ml_uci
-from .libsvm import fetch_libsvm
-from .climate import fetch_climate
 from pathlib import Path
 CELER_PATH = str(Path.home()) + '/celer_data/'
+
+from .climate import fetch_climate
+from .libsvm import fetch_libsvm
+from .ml_uci import fetch_ml_uci
+
