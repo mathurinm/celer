d30cd3c94a9867712d89e0288f9050a901d8ac7c
mathurinm <mathurin.massias@gmail.com>
2020-11-19 17:06:08 +0100

    modifs

--- examples/plot_k_fold.py
+++ examples/plot_k_fold.py
@@ -2,6 +2,7 @@
 Experiment to check how LassoCV's optimal alpha depend on K of K-fold.
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -11,21 +12,24 @@ from celer.plot_utils import configure_plt
 
 configure_plt()
 
-n_samples, n_features, nnz = 500, 500, 100
+n_samples, n_features, nnz = 1000, 1000, 100
 X_, y_, _ = make_correlated_data(
-    n_samples, n_features, nnz=nnz, rho=0.5, snr=5)
+    n_samples, n_features, nnz=nnz, rho=0.5, snr=3)
 
 
 X, X_val, y, y_val = train_test_split(X_, y_, train_size=0.8, random_state=0)
 
 plt.figure(figsize=(6, 6), constrained_layout=True)
 best_alphas = []
+val_mses = []
 Ks = [2, 3, 4, 5, 10]
 for K in Ks:
     clf = LassoCV(cv=K, n_jobs=-1, verbose=True).fit(X, y)
     best_alphas.append(clf.alpha_)
     plt.semilogx(clf.alphas_, clf.mse_path_.mean(
         axis=1), label=r"$K = %d$" % K)
+    val_mses.append(np.mean((y_val - clf.predict(X_val)) ** 2))
+
 plt.legend()
 plt.ylabel("Average MSE on test folds")
 plt.xlabel(r"$\alpha$")
@@ -33,4 +37,12 @@ plt.xlabel(r"$\alpha$")
 
 plt.figure()
 plt.scatter(Ks, best_alphas)
+plt.xlabel("K")
+plt.ylabel(r"best $\alpha")
+
+plt.figure()
+plt.scatter(Ks, val_mses)
+plt.xlabel("K")
+plt.ylabel(r"validation MSE")
+
 plt.show(block=False)
