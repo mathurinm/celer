cb284b5e052fdc40df31fd2c5711ca8474035673
mathurinm <mathurin.massias@gmail.com>
2020-11-20 19:30:42 +0100

    docstring + doc

--- celer/datasets/simulated.py
+++ celer/datasets/simulated.py
@@ -8,19 +8,23 @@ from sklearn.utils import check_random_state
 
 
 def make_correlated_data(n_samples=100, n_features=50, rho=0.6, snr=3,
-                         w_true=None, nnz=10, random_state=None):
+                         density=0.2, w_true=None, random_state=None):
     r"""Generate correlated design matrix with decaying correlation rho**|i-j|.
-    according to:
+    according to
 
-        y = X w^* + noise
+    .. math::
 
-    such that :math:`||X w^*|| / ||noise|| = snr`.
+        y = X w^* + \epsilon
+
+    such that :math:`||X w^*|| / ||\epsilon|| = snr`.
 
     The generated features have mean 0, variance 1 and the expected correlation
     structure:
 
+    .. math::
+
         \mathbb E[x_i] = 0~, \quad \mathbb E[x_i^2] = 1  \quad
-        and \quad \mathbb E[x_ix_j] = \rho^{|i-j|}
+        \text{and} \quad \mathbb E[x_ix_j] = \rho^{|i-j|}
 
 
     Parameters
@@ -30,9 +34,10 @@ def make_correlated_data(n_samples=100, n_features=50, rho=0.6, snr=3,
     n_features: int
         Number of features in the design matrix.
     corr: float
-        Correlation $\rho$ between successive features. The element $C_{i, j}$
-        in the correlation matrix will be $\rho^{|i-j|}$. This parameter
-        should be selected in $[0, 1[$.
+        Correlation :math:`\rho` between successive features. The element
+        :math:`C_{i, j}` in the correlation matrix will be
+        :math:`\rho^{|i-j|}`. This parameter should be selected in
+        :math:`[0, 1[`.
     snr : float
         Signal-to-noise ratio.
     w_true: np.array, shape (n_features,) | None
@@ -55,20 +60,26 @@ def make_correlated_data(n_samples=100, n_features=50, rho=0.6, snr=3,
     """
     if not 0 <= rho < 1:
         raise ValueError("The correlation `rho` should be chosen in [0, 1[.")
+    if not 0 < density <= 1:
+        raise ValueError("The density should be chosen in ]0, 1].")
     rng = check_random_state(random_state)
+    nnz = int(density * n_features)
 
-    # X is generated cleverly using an AR model with reason corr and innovation
-    # sigma^2 = 1 - \rho ** 2: X[:, j+1] = rho X[:, j] + epsilon_j
-    # where  epsilon_j = sigma * np.random.randn(n_samples)
-    sigma = np.sqrt(1 - rho * rho)
-    U = rng.randn(n_samples)
+    if rho == 0:
+        X = np.asfortranarray(np.random.randn(n_samples, n_features))
+    else:
+        # X is generated cleverly using an AR model with reason corr and
+        # innovation sigma^2 = 1 - \rho ** 2: X[:, j+1] = rho X[:, j] + eps_j
+        # where eps_j = sigma * np.random.randn(n_samples)
+        sigma = np.sqrt(1 - rho * rho)
+        U = rng.randn(n_samples)
 
-    X = np.empty([n_samples, n_features], order='F')
-    X[:, 0] = U
-    for j in range(1, n_features):
-        U *= rho
-        U += sigma * rng.randn(n_samples)
-        X[:, j] = U
+        X = np.empty([n_samples, n_features], order='F')
+        X[:, 0] = U
+        for j in range(1, n_features):
+            U *= rho
+            U += sigma * rng.randn(n_samples)
+            X[:, j] = U
 
     if w_true is None:
         w_true = np.zeros(n_features)
