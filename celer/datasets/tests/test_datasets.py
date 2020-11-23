86a2390795048e9f1fa90db569019acc38c08751
mathurinm <mathurin.massias@gmail.com>
2020-11-23 09:53:17 +0100

    test make_correlated data

--- celer/datasets/tests/test_datasets.py
+++ celer/datasets/tests/test_datasets.py
@@ -1,5 +1,17 @@
-# import numpy as np
+import numpy as np
+from numpy.linalg import norm
 
+from celer.datasets import make_correlated_data
+
+
+def test_correlated():
+    X, y, w_true = make_correlated_data(snr=0)
+    np.testing.assert_allclose(y, X @ w_true)
+
+    snr = 5
+    w_true = np.ones(50)
+    X, y, _ = make_correlated_data(n_features=w_true.shape[0], snr=5)
+    np.testing.assert_allclose(snr, norm(X @ w_true), norm(y - X @ w_true))
 
 # # def test_climate():
 #     # X, y = fetch_climate()
