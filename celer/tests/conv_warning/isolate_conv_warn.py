c3bbaf06112557ac16ed827a601c22f0623da16b
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-04-03 11:22:34 +0000

    fit with alpha_max && test with N(0,1)

--- celer/tests/conv_warning/isolate_conv_warn.py
+++ celer/tests/conv_warning/isolate_conv_warn.py
@@ -1,12 +1,36 @@
+import numpy as np
+from numpy.linalg import norm
+
 from celer.dropin_sklearn import LogisticRegression
 from celer.tests.conv_warning.dumped_data import DICT_DATA
 
 
-for check_name, check_props in DICT_DATA.items():
+for check_props in DICT_DATA.values():
     X = check_props['X']
     y = check_props['y']
     C = check_props['C']
     tol = check_props['tol']
 
-    clf = LogisticRegression(C=C, tol=tol, verbose=0)
+    alpha_max = norm(X.T @ y, ord=np.inf)
+    current_alpha = 1. / C
+    # print(f"ratio current_alpha/alpha_max: {current_alpha / alpha_max}")
+
+    # init with alpha_max instead of current_alpha
+    clf = LogisticRegression(C=1/alpha_max, tol=tol, verbose=0)
     clf.fit(X, y)
+
+
+# with a standard normal
+n_samples = 100
+C = 6.
+tol = 1e-4
+
+alpha_max = norm(X.T @ y, ord=np.inf)
+current_alpha = 1. / C
+# print(f"ratio current_alpha/alpha_max: {current_alpha / alpha_max}")
+
+X = np.random.randn(100, 2)
+y = np.random.randint(0, 2, size=100)
+
+clf2 = LogisticRegression(C=C, tol=tol, verbose=0)
+clf2.fit(X, y)
