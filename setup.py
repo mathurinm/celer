756c6210514481942de29c65a92d6814baa06c20
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-04-24 13:02:51 +0200

    update setup.py && licence

--- setup.py
+++ setup.py
@@ -1,33 +1,39 @@
+# Copyright (c) 2018-2022, Celer
+# All rights reserved.
+#
+# License: BSD 3 clause
+
 from setuptools.command.build_ext import build_ext
 from setuptools import dist, setup, Extension, find_packages
 import os
 dist.Distribution().fetch_build_eggs(['numpy>=1.12'])
 import numpy as np  # noqa
 
-descr = 'Fast algorithm with dual extrapolation for sparse problems'
-
-version = None
-with open(os.path.join('celer', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
 
 DISTNAME = 'celer'
-DESCRIPTION = descr
+DESCRIPTION = 'Fast algorithm with dual extrapolation for sparse problems'
 MAINTAINER = 'Mathurin Massias'
 MAINTAINER_EMAIL = 'mathurin.massias@gmail.com'
+
+with open('README.rst', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/mathurinm/celer.git'
-VERSION = version
-URL = 'https://mathurinm.github.io/celer'
 
-setup(name='celer',
+with open(os.path.join('celer', '__init__.py'), 'r') as fid:
+    src = fid.read()
+    exec(src)
+    VERSION = __version__
+
+URL = 'https://mathurinm.github.io/celer'
+CYTHON_MODULES = ['lasso_fast', 'cython_utils',
+                  'PN_logreg', 'multitask_fast', 'group_fast']
+
+setup(name=DISTNAME,
       version=VERSION,
       description=DESCRIPTION,
-      long_description=open('README.rst').read(),
+      long_description=LONG_DESCRIPTION,
       license=LICENSE,
       maintainer=MAINTAINER,
       maintainer_email=MAINTAINER_EMAIL,
@@ -38,31 +44,10 @@ setup(name='celer',
                         'scikit-learn>=1.0', 'xarray', 'download', 'tqdm'],
       packages=find_packages(),
       cmdclass={'build_ext': build_ext},
-      ext_modules=[
-          Extension('celer.lasso_fast',
-                    sources=['celer/lasso_fast.pyx'],
-                    language='c++',
-                    include_dirs=[np.get_include()],
-                    extra_compile_args=["-O3"]),
-          Extension('celer.cython_utils',
-                    sources=['celer/cython_utils.pyx'],
-                    language='c++',
-                    include_dirs=[np.get_include()],
-                    extra_compile_args=["-O3"]),
-          Extension('celer.PN_logreg',
-                    sources=['celer/PN_logreg.pyx'],
-                    language='c++',
-                    include_dirs=[np.get_include()],
-                    extra_compile_args=["-O3"]),
-          Extension('celer.multitask_fast',
-                    sources=['celer/multitask_fast.pyx'],
-                    language='c++',
-                    include_dirs=[np.get_include()],
-                    extra_compile_args=["-O3"]),
-          Extension('celer.group_fast',
-                    sources=['celer/group_fast.pyx'],
-                    language='c++',
-                    include_dirs=[np.get_include()],
-                    extra_compile_args=["-O3"]),
-      ],
+      ext_modules=[Extension(f'celer.{cython_module}',
+                             sources=[f'celer/{cython_module}.pyx'],
+                             language='c++',
+                             include_dirs=[np.get_include()],
+                             extra_compile_args=["-O3"])
+                   for cython_module in CYTHON_MODULES],
       )
