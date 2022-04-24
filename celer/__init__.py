505d6ebd96057a38fdecd50459d56b497506bafe
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-04-24 13:27:51 +0200

    fix __version__ in make html

--- celer/__init__.py
+++ celer/__init__.py
@@ -1,8 +1,9 @@
 """Celer algorithm to solve L1-type regularized problems."""
 
-if __name__ == '__main__':
-    __version__ = '0.7dev'
-else:
+__version__ = '0.7dev'
+
+
+if __name__ != '__main__':
     from .homotopy import celer_path
     from .dropin_sklearn import (Lasso, LassoCV, LogisticRegression, GroupLasso,
                                  GroupLassoCV, MultiTaskLasso, MultiTaskLassoCV)
