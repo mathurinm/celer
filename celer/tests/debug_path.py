e7607a0b280b5b2c10b2b72725152974e43b6bca
mathurinm <mathurin.massias@gmail.com>
2022-04-27 17:54:35 +0200

    pinpoint one issue: solver probably exits too soon because of wrong dual

--- celer/tests/debug_path.py
+++ celer/tests/debug_path.py
@@ -2,7 +2,8 @@
 import numpy as np
 from numpy.linalg import norm
 from numpy.testing import assert_allclose, assert_array_less
-from sklearn.linear_model import (enet_path, ElasticNet as sk_ElasticNet, lasso_path)
+from sklearn.linear_model import (
+    enet_path, ElasticNet as sk_ElasticNet, lasso_path)
 
 from celer import Lasso, ElasticNet, celer_path
 from celer.utils.testing import build_dataset
@@ -17,11 +18,12 @@ l1_ratio = 0.5
 n_alphas = 20
 alpha_max = norm(X.T @ y, ord=np.inf) / (n_samples * l1_ratio)
 params = dict(eps=1e-3, alphas=[alpha_max, alpha_max / 5., alpha_max /
-              25., alpha_max / 625., alpha_max / 3125.], tol=tol, l1_ratio=l1_ratio)
+                                25., alpha_max / 625., alpha_max / 3125.], tol=tol, l1_ratio=l1_ratio)
 
 alphas1, coefs1, gaps1 = celer_path(
     X, y, "lasso", return_thetas=False, verbose=0, prune=prune, **params, max_iter=30)
 
+assert gaps1[-1] > 0
 alphas2, coefs2, _ = enet_path(X, y, verbose=0, **params,
                                max_iter=10000)
 
