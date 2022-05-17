ff16814ab05deaffcb933b1fad41366849ebde61
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2022-05-17 20:12:59 +0200

    scal only if ``alpha_max <= alpha``

--- celer/tests/reproduce.py
+++ celer/tests/reproduce.py
@@ -8,7 +8,7 @@ X, y = libsvmdata.fetch_libsvm("rcv1.binary")
 alpha_max = norm(X.T @ y, ord=np.inf) / len(y)
 
 m = 5
-clf = Lasso(alpha_max*m, fit_intercept=False)
+clf = Lasso(alpha=alpha_max*m, fit_intercept=False)
 clf.fit(X, y)
 
 assert norm(clf.coef_) == 0
